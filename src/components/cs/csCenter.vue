41a6fd1705ac3051fa2f69beb0fe7bc638a81a41
januarry22 <tls2330@naver.com>
2022-07-24 18:39:25 +0900

    질문/답변 페이지 추가 , 콘솔 배너 추가

---
+++ src/components/cs/csCenter.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="cs-wrap">
+      <div class="cs-container">
+         <p> 자주하는 질문 </p>
+      </div>
+    </div>
+    <div>
+      
+    </div>
+</template>
+
+<script>
+import { BIcon } from 'bootstrap-vue'
+import { dragscroll } from 'vue-dragscroll'
+
+export default {
+  //eslint-disable-next-line
+  name: 'csCenter',
+  components: {
+    BIcon,
+  },
+   created: function () {
+            this.getCategory()
+        },
+  methods: {
+    getCategory: function () {
+            this.$axios.get('/api/v1/category/main').then(response => {
+                this.category = response.data.data
+               // console.log('### data: ' + JSON.stringify(this.companyInfo,null,2))
+            }).catch(error => {
+                console.log(error)
+      })
+    }
+  },
+  data: function () {
+    return {
+
+     }
+  },
+
+}
+</script>
+<style scoped>
+  /* @import "../../assets/style.css"; */
+
+.cs-wrap{
+    background-color: #eae9e9;
+    position: relative;
+    height: 118px;
+    border-radius: 15px;
+    margin: 20px;
+}
+.cs-container{
+   width: 100%;
+    height: 100%;
+}
+</style>
