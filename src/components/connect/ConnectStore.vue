9804f7233c87e3d8680f6fad8940031682bebce9
januarry22 <tls2330@naver.com>
2022-07-27 16:49:42 +0900

    맞춤업체 페이지

---
+++ src/components/connect/ConnectStore.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="index">
+      <div>
+      <b-tabs class="slide-menu">
+       <b-tab title="프로필" active>
+         <MyProfile></MyProfile>
+       </b-tab>
+        <b-tab title="나의 쇼핑">
+         <MyShopping></MyShopping>
+        </b-tab>
+      </b-tabs>
+      </div>
+    </div>
+</template>
+
+<script>
+import { BIcon } from 'bootstrap-vue'
+import MyProfile from '@/components/mypage/MyProfile';
+import MyShopping from '@/components/mypage/MyShopping';
+
+export default {
+  //eslint-disable-next-line
+  name: 'MyPage',
+  components: {
+    BIcon,
+    MyProfile,
+    MyShopping
+  },
+   created: function () {
+  },
+  methods: {
+    
+  },
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.index{
+    background-color: #ffffff;
+}
+.slide-menu .nav.nav-tabs {
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+    padding: 0 10px;
+    justify-content: space-around;
+}
+
+
+</style>
